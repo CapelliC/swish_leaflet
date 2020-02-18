7d1c0f96837a08879670ed9565b0da35204e59a1
CapelliC <cc.carlo.cap@gmail.com>
2020-02-18 22:34:52 +0100

    test

--- swish/lib/render/leaflet.pl
+++ swish/lib/render/leaflet.pl
@@ -39,7 +39,6 @@
 :- use_module(library(http/html_write)).
 :- use_module(library(http/js_write)).
 :- use_module('../render').
-:- use_module(library(option)).
 :- use_module(library(debug)).
 
 :- use_module(config(plugin_leaflet)).
@@ -50,43 +49,61 @@
 Render geomap data as an interactive Leaflet map.
 */
 
+options_defaults(OptionsIn, Options) :-
+  maplist({OptionsIn}/[Opt]>>(
+      Opt =.. [Key,Def,Var],
+      Vin =.. [Key,Var],
+      ( memberchk(Vin, OptionsIn) -> true ; Var = Def )
+    ), Options).
+
 %%  term_rendering(+Term, +Vars, +Options)//
 %
 %   Renders Term as a Leaflet map.
 %   I'm pleased to see how easy is to pass JSON to
 %   js_script//1, using quasi-quotation
 %
-term_rendering(_Term,_Vars,Options) -->
+term_rendering(Term,Vars,OptionsIn) -->
   {
-    select_option(width(Width),Options,Options1,'400px'),
-    select_option(height(Height),Options1,Options2,'400px'),
-    select_option(backgroundColor(BackgroundColor),Options2,_Options3,ivory),
-
-    LlUrl='/plugin/leaflet/leaflet.js'
+    Options = [
+      id(mapid,Id),
+      width('400px',Width),
+      height('400px',Height),
+      backgroundColor(ivory,BackgroundColor)
+    ],
+    options_defaults(OptionsIn, Options),
+    debug(leaflet, 'Term:~w Vars:~w Options:~w', [Term,Vars,Options])
   },
-  html(div([ id(mapid), class(['render-leaflet']),
+  html(div([ id(Id), class(['render-leaflet']),
         'data-render'('Leaflet geomap')
       ],
-      [ \js_script({|javascript(LlUrl,Width,Height,BackgroundColor)||
+      [ \js_script({|javascript(Id,Width,Height,BackgroundColor)||
 (function() {
   var trace = console.log
   if ($.ajaxScript) {
 
     var l_cont = $.ajaxScript.parent()
-
+    var $c = $(l_cont)
     // fix div size
-    $(l_cont).css('height', Height)
-    $(l_cont).css('width', Width)
+    if (Height == 'auto') {
+      $c.css('height', $c.css('width'))
+    } else {
+      $c.css('height', Height)
+    }
+    if (Width == 'auto') {
+      $c.css('width', $c.css('height'))
+    } else {
+      $c.css('width', Width)
+    }
 
     // just to inspect the render area
-    $(l_cont).css('background-color', BackgroundColor)
+    $c.css('background-color', BackgroundColor)
 
-    require([LlUrl], function(L_r) {
-trace('running leaflet', L, L_r)
+    require(['/plugin/leaflet/leaflet.js'], function(L) {
+trace('leaflet instance', L)
       try {
         var provider = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13)
-        
+        var mymap = L.map(Id).setView([0, 0], 1)
+
         L.tileLayer(provider, {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
@@ -95,10 +112,10 @@ trace('running leaflet', L, L_r)
             zoomOffset: -1,
             //accessToken: 'your.mapbox.access.token'
         }).addTo(mymap)
-        
+
       }
       catch(e) {
-        $(l_cont).html(e)
+        $c.html(e)
       }
     })
   }
